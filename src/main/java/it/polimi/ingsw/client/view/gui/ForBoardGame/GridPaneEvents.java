78852763e91a9974f177a38dd16c9bd7b57d02a9
G <itsmulanyall@gmail.com>
2022-09-15 00:29:11 +0200

    Add files via upload

---
+++ src/main/java/it/polimi/ingsw/client/view/gui/ForBoardGame/GridPaneEvents.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.client.view.gui.ForBoardGame;
+
+public enum GridPaneEvents {
+    SETTING_ALL_BUTTONS,
+    SETTING_BOARD,
+    AVAILABLE_POSITIONS,
+    PLACE_MALE_WORKER,
+    PLACE_FEMALE_WORKER;
+}
